331f31e3c924007e84bdd00a7f6cc43dcc708f5d
ttzytt <Zyt2006613@outlook.com>
2022-12-06 10:50:49 -0500

    add image panel

---
+++ src/utils/imagePanel.java
@@ -0,0 +1,121 @@
+package utils;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class imagePanel extends JPanel {
+    private BufferedImage dspImg;
+    private BufferedImage origImg;
+    private int owid, ohei;
+    private double aspRate;
+
+    public imagePanel(String path) {
+        try {
+            dspImg = ImageIO.read(new File(path));
+            origImg = ImageIO.read(new File(path));
+            owid = origImg.getWidth();
+            ohei = origImg.getHeight();
+            aspRate = (double) owid / (double) ohei;
+        } catch (IOException ex) {
+            System.out.println("exception thrown when opening image file: " + ex.getMessage());
+            // handle exception...
+        }
+    }
+
+    public void resize(int wid, int hei) {
+        Image tmp = origImg.getScaledInstance(wid, hei, Image.SCALE_SMOOTH);
+        dspImg = new BufferedImage(wid, hei, BufferedImage.TYPE_INT_RGB);
+        dspImg.getGraphics().drawImage(tmp, 0, 0, null);
+    }
+
+    public void resize(Dimension d) {
+        resize(d.width, d.height);
+    }
+
+    public imagePanel(String path, int wid, int hei) {
+        try {
+            dspImg = ImageIO.read(new File(path));
+            origImg = ImageIO.read(new File(path));
+            owid = origImg.getWidth();
+            ohei = origImg.getHeight();
+            aspRate = (double) owid / (double) ohei;
+        } catch (IOException ex) {
+            // handle exception...
+        }
+        resize(wid, hei);
+    }
+
+    public BufferedImage getDspImg() {
+        return dspImg;
+    }
+
+    public void setDspImg(BufferedImage dspImg) {
+        this.dspImg = dspImg;
+    }
+
+    public BufferedImage getOrigImg() {
+        return origImg;
+    }
+
+    public void setOrigImg(BufferedImage origImg) {
+        this.origImg = origImg;
+    }
+
+    public int getOwid() {
+        return owid;
+    }
+
+    public void setOwid(int owid) {
+        this.owid = owid;
+    }
+
+    public int getOhei() {
+        return ohei;
+    }
+
+    public void setOhei(int ohei) {
+        this.ohei = ohei;
+    }
+
+    public double getAspRate() {
+        return aspRate;
+    }
+
+    public void setAspRate(double aspRate) {
+        this.aspRate = aspRate;
+    }
+
+    public BufferedImage getBufImg() {
+        return dspImg;
+    }
+
+    public void setBufImg(BufferedImage bufImg) {
+        this.dspImg = bufImg;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Dimension dependW = new Dimension(getWidth(), (int) Math.round((double) getWidth() / aspRate));
+        Dimension dependH = new Dimension((int) Math.round((double) getHeight() * aspRate), getHeight());
+
+        if (dependW.height <= getHeight()) {
+            // resize the image but make sure that the asp ratio is correct
+            resize(dependW);
+        } else {
+            resize(dependH);
+        }
+
+        // default is to center the image
+        int wOff = getWidth() - dspImg.getWidth();
+        int hOff = getHeight() - dspImg.getHeight();
+
+        g.drawImage(dspImg, (int) Math.round((double) wOff / 2.0), (int) Math.round((double) hOff / 2.0), this);
+    }
+}
