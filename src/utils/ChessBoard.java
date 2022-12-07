c966431fb57f2feebdd8c7ebb35d062c9de26d8f
craneMaster <119700571+craneMaster@users.noreply.github.com>
2022-12-07 14:17:51 -0500

    Update ChessBoard.java

--- src/utils/ChessBoard.java
+++ src/utils/ChessBoard.java
@@ -14,6 +14,31 @@ public class ChessBoard {
             board[6][i] = new Pawn(false);
             //// board[7][i] = new Piece(SETUP_LAYOUT[i], true);
         }
+        board[0][0] = new Rook(true);
+        board[0][7] = new Rook(true);
+        board[7][0] = new Rook(false);
+        board[7][7] = new Rook(false);
+
+        board[0][1] = new Knight(true);
+        board[0][6] = new Knight(true);
+        board[7][1] = new Knight(false);
+        board[7][6] = new Knight(false);
+
+        board[0][2] = new Bishop(true);
+        board[0][5] = new Bishop(true);
+        board[7][2] = new Bishop(false);
+        board[7][5] = new Bishop(false); 
+
+        board[0][3] = new King(true);
+        board[0][4] = new Queen(true);
+        board[7][3] = new King(false);
+        board[7][4] = new Queen(false);
+    }
+
+    public submitMove(Move themove){
+        board[themove.end[0]][themove.end[1]] = themove.piece;
+        board[themove.start[0]][themove.start[1]] = null;
+        if(themove.capture!=null) board[themove.capture[0]][themove.capture[1]] = null;
     }
 
     public Piece[][] getBoard() {
