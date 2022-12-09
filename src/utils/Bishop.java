23daa8bf2894ff53253f72fc9ebfa032322de524
craneMaster <119700571+craneMaster@users.noreply.github.com>
2022-12-09 13:34:41 -0500

    Update Bishop.java

--- src/utils/Bishop.java
+++ src/utils/Bishop.java
@@ -7,51 +7,55 @@ public class Bishop extends Piece {
 	public Bishop(boolean color) {
 		super(color);
 	}
-
+	
 	public ArrayList<int[]> getMoveSet(Piece[][] board, int x, int y) {
 		ArrayList<int[]> moves = new ArrayList<>();
 		
 		for(int i = 1; i <= 8; i++) {
-			if(x + i >= board[0].length || y + i >= board[0].length || board[x + i][y + i] != null){
-				if ( board[ x + i ][ y + i ].getColor() != this.getColor() ){
-					int[] temp = {x + i, y + i};
-					moves.add(temp);
-				}
+			if(x + i >= board[0].length || y + i >= board[0].length)break;
+				if(board[x + i][y + i] != null){
+					if ( board[ x + i ][ y + i ].getColor() != this.getColor() ){
+						int[] temp = {x + i, y + i};
+						moves.add(temp);
+					}
 				break;
-			}
+				}
 			int[] temp = {x + i, y + i};
 			moves.add(temp);
 		}
 		for(int i = 1; i <= 8; i++) {
-			if(x + i >= board[0].length || y - i < 0 || board[x + i][y - i] != null){
-				if ( board[ x + i ][ y - i ].getColor() != this.getColor() ){
-					int[] temp = {x + i, y - i};
-					moves.add(temp);
+			if(x + i >= board[0].length || y - i < 0)break;
+				if(board[x + i][y - i] != null){
+					if ( board[ x + i ][ y - i ].getColor() != this.getColor() ){
+						int[] temp = {x + i, y - i};
+						moves.add(temp);
+					}
+					break;
 				}
-				break;
-			}
 			int[] temp = {x + i, y - i};
 			moves.add(temp);
 		}
 		for(int i = 1; i <= 8; i++) {
-			if(x - i < 0 || y + i >= board[0].length || board[x - i][y + i] != null){
-				if ( board[ x - i ][ y + i ].getColor() != this.getColor() ){
-					int[] temp = {x - i, y + i};
-					moves.add(temp);
+			if(x - i < 0 || y + i >= board[0].length)break;
+				if(board[x - i][y + i] != null){
+					if ( board[ x - i ][ y + i ].getColor() != this.getColor() ){
+						int[] temp = {x - i, y + i};
+						moves.add(temp);
+					}
+					break;
 				}
-				break;
-			}
 			int[] temp = {x - i, y + i};
 			moves.add(temp);
 		}
 		for(int i = 1; i <= 8; i++) {
-			if(x - i < 0 || y - i < 0 || board[x - i][y - i] != null){
-				if ( board[ x - i ][ y - i ].getColor() != this.getColor() ){
-					int[] temp = {x - i, y - i};
-					moves.add(temp);
+			if(x - i < 0 || y - i < 0 )break;
+				if(board[x - i][y - i] != null){
+					if ( board[ x - i ][ y - i ].getColor() != this.getColor() ){
+						int[] temp = {x - i, y - i};
+						moves.add(temp);
+					}
+					break;
 				}
-				break;
-			}
 			int[] temp = {x - i, y - i};
 			moves.add(temp);
 		}
