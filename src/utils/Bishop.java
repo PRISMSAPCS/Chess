e5b7627b4ec808e8c61be6c12af93916d6576542
craneMaster <119700571+craneMaster@users.noreply.github.com>
2022-12-08 21:09:32 -0500

    Update Bishop.java

--- src/utils/Bishop.java
+++ src/utils/Bishop.java
@@ -3,21 +3,34 @@ package utils;
 public class Bishop extends Piece {
   
      public ArrayList<int[]> getMoveSet(Piece[][] board, int x, int y) {
-       ArrayList<int[]> moves = new ArrayList<>();
-       int dx,dy;
-       int[][] positions = {{1,2},{2,1},{1,-2},{-2,1},{2,-1},{-1,2},{-1,-2},{-2,-1}}
-       for(int i=0;i<8;i++){
-         dx = positions[i][0];
-         dy = positions[i][1];
-         if( x+dx<8 && x+dx>=0 && y+dy<8 && y+dy>=0 ){
-           if(board[x+dx][y+dy]==null){
-             int[] temp = {x+dx,y+dy};
-             moves.add(temp);
-           }
-         }
-       }
+		ArrayList<int[]> moves = new ArrayList<>();
 		
+		for(int i = 1; i <= 8; i++) {
+			if(x + i >= board[0].length || y + i >= board[0].length || board[x + i][y + i] != null)
+				break;
+			int[] temp = {x + i, y + i};
+			moves.add(temp);
+		}
+		for(int i = 1; i <= 8; i++) {
+			if(x + i >= board[0].length || y - i < 0 || board[x + i][y - i] != null)
+				break;
+			int[] temp = {x + i, y - i};
+			moves.add(temp);
+		}
+		for(int i = 1; i <= 8; i++) {
+			if(x - i < 0 || y + i >= board[0].length || board[x - i][y + i] != null)
+				break;
+			int[] temp = {x - i, y + i};
+			moves.add(temp);
+		}
+		for(int i = 1; i <= 8; i++) {
+			if(x - i < 0 || y - i < 0 || board[x - i][y - i] != null)
+				break;
+			int[] temp = {x - i, y - i};
+			moves.add(temp);
+		}
 		
-     return moves;
-  }
+		return moves;
+	}
+	
 }
