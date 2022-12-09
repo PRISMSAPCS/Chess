5b91c90eeb4137c21c88fae7c8358a42d9401c74
craneMaster <119700571+craneMaster@users.noreply.github.com>
2022-12-08 21:08:02 -0500

    Update Knight.java

--- src/utils/Knight.java
+++ src/utils/Knight.java
@@ -11,9 +11,21 @@ public class Knight extends Piece {
 	@Override
 	public ArrayList<int[]> getMoveSet(Piece[][] board, int x, int y) {
 		ArrayList<int[]> moves = new ArrayList<>();
-		// TODO
+		//// TODO
+		int dx,dy;
+                int[][] positions = {{1,2},{2,1},{1,-2},{-2,1},{2,-1},{-1,2},{-1,-2},{-2,-1}}
+                for(int i=0;i<8;i++){
+                 dx = positions[i][0];
+                 dy = positions[i][1];
+                if( x+dx<8 && x+dx>=0 && y+dy<8 && y+dy>=0 ){
+                if(board[x+dx][y+dy]==null){
+                 int[] temp = {x+dx,y+dy};
+                moves.add(temp);
+           }
+         }
 		return moves;
 	}
+	
 
 	@Override
 	public String getIconFile() {
