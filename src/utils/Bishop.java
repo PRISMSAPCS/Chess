4a4b182b8d8b972935e099c5a6c826de4dc2f9cc
craneMaster <119700571+craneMaster@users.noreply.github.com>
2022-12-09 11:14:56 -0500

    Update Bishop.java

--- src/utils/Bishop.java
+++ src/utils/Bishop.java
@@ -12,26 +12,46 @@ public class Bishop extends Piece {
 		ArrayList<int[]> moves = new ArrayList<>();
 		
 		for(int i = 1; i <= 8; i++) {
-			if(x + i >= board[0].length || y + i >= board[0].length || board[x + i][y + i] != null)
+			if(x + i >= board[0].length || y + i >= board[0].length || board[x + i][y + i] != null){
+				if ( board[ x + i ][ y + i ].getColor() != this.getColor() ){
+					int[] temp = {x + i, y + i};
+					moves.add(temp);
+				}
 				break;
+			}
 			int[] temp = {x + i, y + i};
 			moves.add(temp);
 		}
 		for(int i = 1; i <= 8; i++) {
-			if(x + i >= board[0].length || y - i < 0 || board[x + i][y - i] != null)
+			if(x + i >= board[0].length || y - i < 0 || board[x + i][y - i] != null){
+				if ( board[ x + i ][ y - i ].getColor() != this.getColor() ){
+					int[] temp = {x + i, y - i};
+					moves.add(temp);
+				}
 				break;
+			}
 			int[] temp = {x + i, y - i};
 			moves.add(temp);
 		}
 		for(int i = 1; i <= 8; i++) {
-			if(x - i < 0 || y + i >= board[0].length || board[x - i][y + i] != null)
+			if(x - i < 0 || y + i >= board[0].length || board[x - i][y + i] != null){
+				if ( board[ x - i ][ y + i ].getColor() != this.getColor() ){
+					int[] temp = {x - i, y + i};
+					moves.add(temp);
+				}
 				break;
+			}
 			int[] temp = {x - i, y + i};
 			moves.add(temp);
 		}
 		for(int i = 1; i <= 8; i++) {
-			if(x - i < 0 || y - i < 0 || board[x - i][y - i] != null)
+			if(x - i < 0 || y - i < 0 || board[x - i][y - i] != null){
+				if ( board[ x - i ][ y - i ].getColor() != this.getColor() ){
+					int[] temp = {x - i, y - i};
+					moves.add(temp);
+				}
 				break;
+			}
 			int[] temp = {x - i, y - i};
 			moves.add(temp);
 		}
