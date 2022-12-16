c889d96cb1e0a06878c69bfdba46c3aaad664781
ttzytt <Zyt2006613@outlook.com>
2022-12-16 02:56:15 -0500

    implements the basics of logging, still have a lot of special cases to work on, such as promotion and en passant

--- src/utils/Main.java
+++ src/utils/Main.java
@@ -3,8 +3,10 @@ package utils;
 public class Main {
     public static void main(String[] args){
         ChessBoard board = new ChessBoard();
+        // board.enableLogging();
         GUI gui = new GUI(board);
         while(true) {
+
             Move move = gui.getMove();
             board.submitMove(move);
             gui.applyMove(move);
